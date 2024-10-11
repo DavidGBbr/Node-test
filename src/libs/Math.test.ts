60287c3d63ba3d2580ea904b40895ff912fae1a4
davidGBbr <davidgomesbrigido@gmail.com>
2024-10-11 09:50:44 -0300

    Criando primeiro teste

---
+++ src/libs/Math.test.ts
@@ -0,0 +1,26 @@
+import { Math } from "./Math";
+
+describe("Testing Math library", () => {
+  it("should sum two numbers correctly", () => {
+    const response = Math.sum(5, 10);
+    expect(response).toBe(15);
+  });
+
+  it("should subtract two numbers correctly", () => {
+    const response = Math.sub(10, 5);
+    expect(response).toBe(5);
+  });
+
+  it("should multiply two numbers correctly", () => {
+    const response = Math.mult(5, 10);
+    expect(response).toBe(50);
+  });
+
+  it("should divide two numbers correctly", () => {
+    const response = Math.div(10, 2);
+    expect(response).toBe(5);
+
+    const response2 = Math.div(3, 0);
+    expect(response2).toBe(false);
+  });
+});
