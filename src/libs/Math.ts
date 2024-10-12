ccb49b23c60ce2625605d01e8f6934ca651c029e
davidGBbr <davidgomesbrigido@gmail.com>
2024-10-12 00:18:58 -0300

    Criando funcao a partir do teste

--- src/libs/Math.ts
+++ src/libs/Math.ts
@@ -1,6 +1,14 @@
 export const Math = {
-  sum: (n1: number, n2: number) => {},
-  sub: (n1: number, n2: number) => {},
-  div: (n1: number, n2: number) => {},
-  mult: (n1: number, n2: number) => {},
+  sum: (n1: number, n2: number) => {
+    return n1 + n2;
+  },
+  sub: (n1: number, n2: number) => {
+    return n1 - n2;
+  },
+  div: (n1: number, n2: number) => {
+    return n2 == 0 ? false : n1 / n2;
+  },
+  mult: (n1: number, n2: number) => {
+    return n1 * n2;
+  },
 };
