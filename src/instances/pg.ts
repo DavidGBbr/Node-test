bb31ddd3bc21d5595c331515ab6a13cf01e29d2b
davidGBbr <davidgomesbrigido@gmail.com>
2024-10-17 08:03:18 -0300

    Preparando ambiente para testes no banco de dados

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -1,14 +1,10 @@
-import { Sequelize } from 'sequelize'; 
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import db from "./database";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.PG_DB as string,
-    process.env.PG_USER as string,
-    process.env.PG_PASSWORD as string,
-    {
-        dialect: 'postgres',
-        port: parseInt(process.env.PG_PORT as string)
-    }
-);
+export const sequelize = new Sequelize(db.db, db.user, db.password, {
+  dialect: "postgres",
+  port: parseInt(db.port),
+});
